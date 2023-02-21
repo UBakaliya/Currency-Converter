a2013467792af0ffeecac7f7430f7dd649bcfbf8
ubaka2 <ubaka2@uic.edu>
2023-02-21 15:09:50 -0600

    All the files are ready to write in

---
+++ src/test/java/Tests.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Tests {
+    @Test
+    void TempTest() {
+        assertEquals(1, 1);
+    }
+}
